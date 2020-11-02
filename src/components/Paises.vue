77b274ae559e627dc0a2cacac91e2bb57f905988
Benjamin Fernandez <benfernandez.dev@gmail.com>
2020-11-01 20:18:45 -0600

    Tercer Commit: Diseño de plantilla donde se ejecutara la aplicacion

---
+++ src/components/Paises.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="row">
+            <input type="text" class="form-control col-12 col-md-9" placeholder="Buscar...">
+             <b-form-select class="col-12 col-md-3"></b-form-select>
+        </div>
+
+    <div class="row mt-5">
+      <div class="col-6 col-md-4">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="" class="card-img" alt="" />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">El Salvador</h5>
+                <p><b>Capital: </b></p>
+                <p><b>Region: </b></p>
+                <p><b>Sub-Region: </b></p>
+                <p class="card-text">
+                  <b>Pobliacion: </b><small class="text-muted"></small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'Paises'
+}
+</script>
